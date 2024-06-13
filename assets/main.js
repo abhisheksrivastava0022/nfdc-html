0641b724e2efe43c3b369758b2f2ae7e8d3cf510
Ayush Tyagi <ayush_tyagi@sislinfotech.com>
2024-06-13 11:37:30 +0530

    changes in press and aboutus

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,7 @@ $(document).ready(function () {
     $('#dtBasicExample').DataTable();
     $('.dataTables_length').addClass('bs-select');
   });
+  // Initialization for ES Users
+// import { Collapse, initMDB } from 'mdb-ui-kit';
+
+// initMDB({ Collapse });
