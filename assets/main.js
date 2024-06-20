5d4d93f746443775b434613b9792d1c0e93ac5eb
Ayush Tyagi <ayush_tyagi@sislinfotech.com>
2024-06-20 15:50:58 +0530

    change in index html and intercinema page add

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,17 @@ import { Collapse, initMDB } from 'mdb-ui-kit';
 
 initMDB({ Collapse });
 
+
+
+
 import { Input, initMDB } from 'mdb-ui-kit';
 initMDB({ Input });
+
+
+// Initialization for ES Users
+import { Tab, initMDB } from "mdb-ui-kit";
+
+initMDB({ Tab });
+
 
 
